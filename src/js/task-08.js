4b3e3eb03c6081658f669b8bb2957074d658346b
Futozhka <124589958+Futozhka@users.noreply.github.com>
2023-11-07 10:54:45 +0000

    finish

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault(); 
+  const formData = new FormData(loginForm); 
+
+  const userData = {};
+  formData.forEach((value, key) => {
+    userData[key] = value;
+  });
+
+  if (!userData.email || !userData.password) {
+    alert('Please fill in all fields.'); 
+  } else {
+    console.log(userData); 
+    loginForm.reset(); 
+  }
+});
 
